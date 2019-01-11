562e3c2b6f3252b873df977e79bf5b788affe185
dileya <cujaia22@gmail.com>
2019-01-11 18:15:33 +0200

    add hmework1.js

---
+++ homework1.js
@@ -0,0 +1,14 @@
+console.log('Hello Wolrd'); 
+
+var w = 'Hello Wolrd'; 
+console.log(w); 
+
+var helloWolrd; 
+console.log(helloWolrd); 
+
+var h = 'Hello'; 
+var w = 'World'; 
+console.log(h+w); 
+
+var h = 'Hello'; 
+console.log(h+' World');
