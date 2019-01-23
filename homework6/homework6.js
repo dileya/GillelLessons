bbf620b8bf15888b2d38d257fd8222ebfb029d72
dileya <cujaia22@gmail.com>
2019-01-23 20:55:21 +0200

    added homework

---
+++ homework6/homework6.js
@@ -0,0 +1,47 @@
+//Задание 1
+var userIsHappy = 0;
+while (userIsHappy<10) {
+    alert('Are you happy what ' + userIsHappy++ + ' is?');
+};
+
+var userIsHappy = 0;
+do {
+    alert('Are you happy what ' + userIsHappy++ + ' is?');
+}
+while (userIsHappy<10);
+
+//Задание 2
+var randomNumber = Math.round (Math.random() * 10);
+console.log(randomNumber);
+function n () {
+    return prompt('Enter the number from 1 to 10');
+}
+var result;
+for (var number = n ();
++number !== randomNumber; 
+number = n()) {
+    result = confirm('Do you want to try again?');
+    if (!result) break;
+}
+if (+number === randomNumber) {
+    alert('You win!');
+}
+
+//Задание 3
+for (var number = prompt("Enter a number"), i = 1; i % +number; i++) {
+    var result = i * +number;
+    if (!(i % +number)) continue;
+    console.log(result);
+}
+
+//Задание 4
+var arr;
+for (var number = prompt("Enter a number"), quantity = prompt("Enter the quantity"), i = 1;
+     i <= quantity;
+     i++) {
+    var result = [i * +number];
+    if (!(i % +number)) continue;
+    console.log(result);
+}
+
+
---
+++ homework6/main.js
@@ -0,0 +1,42 @@
+
+/*var number = prompt('Enter the number from 1 to 10');
+var randomNumber = Math.round (Math.random * 10);
+var result = confirm('Do you want to try again?');
+while () {
+    var result = confirm('Do you want to try again?');
+    if (!result)  break;
+}
+while (number === randomNumber) {
+    alert('You win!');
+}*/
+/*var randomNumber = Math.round (Math.random() * 10);
+console.log(randomNumber);
+function n () {
+    return prompt('Enter the number from 1 to 10');
+}
+var result;
+for (var number = n ();
++number !== randomNumber; 
+number = n()) {
+    result = confirm('Do you want to try again?');
+    if (!result) break;
+}
+if (+number === randomNumber) {
+    alert('You win!');
+}*/
+
+//Запросить у пользователя значение, кратное которому будут выводиться числа - вывести их в консоль.
+var arr;
+for (var number = prompt("Enter a number"), quantity = prompt("Enter the quantity"), i = 1;
+     i <= quantity;
+     i++) {
+    var result = [i * +number];
+    if (!(i % +number)) continue;
+    console.log(result);
+}
+/*
+var arr = ["Яблоко", "Апельсин", "Груша"];
+
+for (var i = 0; i < arr.length; i++) {
+  alert( arr[i] );
+}*/
