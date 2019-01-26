b02716e04a70fa5d3088d676c9cd3baf998e56d8
dileya <cujaia22@gmail.com>
2019-01-26 18:34:37 +0200

    homework done

--- homework6/main.js
+++ homework6/main.js
@@ -26,10 +26,53 @@ if (+number === randomNumber) {
 }*/
 
 //Запросить у пользователя значение, кратное которому будут выводиться числа - вывести их в консоль.
-for (var number = prompt("Enter a number"), quantity = prompt("Enter the quantity"), i = 0;
+/*for (var number = prompt("Enter a number"), quantity = prompt("Enter the quantity"), i = 0;
      i <= quantity;
      i++) {
     var result = [i * +number];
     if (!(i % +number)) continue;
     console.log(result);
+}*/
+
+/*var key = prompt('Enter the key');
+var definition = prompt('Enter the definition');
+var obj = {};
+obj[key] = definition;
+console.log (obj);*/
+
+function random() {
+    var randomString = "";
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    for (var i = 0; i < alphabet.length; i++)
+    randomString += alphabet.charAt(Math.floor(Math.random() * (alphabet.length -1)));
+    return randomString;
 }
+for (var number = +prompt("Enter the number of results"),
+         max = +prompt('Enter the max'),
+         min = +prompt('Enter the min'), i = 0; 
+         i < number;
+         i++) {
+    var newArr = random();
+    var stringSize = (max + min) / 2;
+    var lastArr = newArr.split('', stringSize);
+    var result = [lastArr].join('');
+    var obj = {[stringSize] : [result]};
+    console.log(result);
+    if (number === i) break;
+}
+alert(number + ' words with ' + stringSize + ' characters');
+console.log(obj);
+
+/*
+for (var number = +prompt("Enter the number of results", result; result <= number;  )
+)
+/*
+    if (newArr.length > stringSize) {
+    newArr.length = stringSize;
+    }*/
+
+
+/*var string = "abcdefghijklmnopqrstuvwxyz";
+var number = '3';
+var arr = string.split('', number);
+console.log (arr);*/
