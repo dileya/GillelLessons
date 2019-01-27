fb63d50bfc28791e522b7529b75e1cf62ae6e7b0
dileya <cujaia22@gmail.com>
2019-01-27 03:04:44 +0200

    add object

--- homework7/homework7.js
+++ homework7/homework7.js
@@ -6,16 +6,21 @@ function random() {
     randomString += alphabet.charAt(Math.floor(Math.random() * (alphabet.length -1)));
     return randomString;
 }
+var obj = {};
 for (var number = +prompt("Enter the number of results"),
          max = +prompt('Enter the max'),
-         min = +prompt('Enter the min'), i = 0; 
+         min = +prompt('Enter the min'), i = 0;
          i < number;
          i++) {
     var newArr = random();
-    var stringSize = (max + min) / 2;
+    var stringSize = Math.round((max + min) / 2);
     var lastArr = newArr.split('', stringSize);
-    var result = [lastArr].join('');
-    var obj = {[stringSize] : [result]};
+    var result = lastArr.join('');
+    if (!obj[stringSize]) {
+        obj[stringSize] = [];
+    }
+    obj[stringSize].push(result);
+    console.log(obj);
     console.log(result);
     if (number === i) break;
 }
