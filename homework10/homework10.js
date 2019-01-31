eed420068779a78cde9ccb13e9df6b3b7a9d2f9a
dileya <cujaia22@gmail.com>
2019-01-31 20:10:21 +0200

    wrote the function, wooohooo!

--- homework10/homework10.js
+++ homework10/homework10.js
@@ -1,56 +1,52 @@
-/*
-Написать скрипт, который будет выводить часы на экран. 
-Предварительно создать несколько тасок. 
-В случае, если время для данной таски пришло - вывести ее название в документе 
-В документе можно вывести с помощью команды document.body.innerText = 'Your text!'; */
-function alarm() {
-  var currentDate = new Date();
-  var startDate = new Date(2019, 31, 01, 01, 36, 0);
-  console.log(currentDate);
-  console.log(startDate);
-if (Math.floor(startDate.getDate() / 1000) === Math.floor(currentDate.getDate() / 1000))
+//Интерактивные часы вывеены на экран скриптом, написанным в index.html
 
-return alert('Task №1 should be done!')
+function alarm(currentDate, startDateOne, startDateTwo, startDateThree) {
+
+var currentDate = new Date();
+var startDateOne = new Date(2019, 0, 31, 20, 7, 0);
+var startDateTwo = new Date(2019, 0, 31, 20, 8, 0);
+var startDateThree = new Date(2019, 0, 31, 20, 9, 0);
+
+ if (Math.floor(currentDate.getTime() / 1000) === Math.floor(startDateOne.getTime() / 1000)) {
+  var a = alert('TaskOne should be done!'); 
+  console.log(taskOne());
+ }
+ if (Math.floor(currentDate.getTime() / 1000) === Math.floor(startDateTwo.getTime() / 1000)) {
+  var a = alert('TaskTwo should be done!'); 
+  console.log(taskTwo());
+ }
+ if (Math.floor(currentDate.getTime() / 1000) === Math.floor(startDateThree.getTime() / 1000)) {
+  var a = alert('TaskThree should be done!'); 
+  console.log(taskThree());
+ }
 }
-alarm();
+  var result = setInterval(function() {
+    alarm();
+    }, 1000);
 
-/*function newTask(title, description, startDate, endDate) {
-  var startDate = new Date();
-  var endDate = new Date();
-  startDate.setHours(15, 30, 15);
-  endDate.setHours(19, 30, 15);
+function taskOne() {
+  var endDate = new Date(2019, 0, 31, 21, 00, 0);
   return {
     title : 'Задача №1',
     description : 'Выучить английский',
-    start : startDate,
     end : endDate
   }
 }
-console.log(newTask());
 
-function otherTask(title, description, startDate, endDate) {
-  var startDate = new Date();
-  var endDate = new Date();
-  startDate.setHours(22, 45, 15, 0);
-  endDate.setHours(21, 30, 15, 0);
+function taskTwo() {
+  var endDate = new Date(2019, 0, 31, 21, 45, 0);
   return {
     title : 'Задача №2',
     description : 'Выучить прототипы',
-    start : startDate,
     end : endDate
   }
 }
-console.log(otherTask());
 
-function anotherTask(title, description, startDate, endDate) {
-  var startDate = new Date();
-  var endDate = new Date();
-  endDate.setDate(startDate.getDate() + 1);
+function taskThree() {
+  var endDate = new Date(2019, 0, 31, 22, 30, 0);
   return {
     title : 'Задача №3',
     description : 'Полежать :)',
-    start : startDate,
     end : endDate
   }
 }
-console.log(anotherTask());*/
